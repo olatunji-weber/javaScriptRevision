81913ba88b67b869a303ed0f0ba556f237a00677
olatunji-weber <olatunji.weber@gmail.com>
2022-09-30 01:25:28 +0200

    another commit

--- revisions.js
+++ revisions.js
@@ -1,18 +1,3 @@
-// let  sum = (num1, num2) => num1 + num2;
-
-// console.log("The result is: " + sum(100, 300));
-
-// let  product = (num1, num2) => num1 * num2;
-
-// console.log("The result is: " + product(100, 300));
-
-// // let raisedToPower = (num, pow) => num ** pow;
-// let raisedToPower = (num, exp) => Math.pow(num, exp);
-// let num1 = 5;
-// let num2 = 2;
-// power = raisedToPower(num1, num2);
-// console.log(`${num1} raised to the power of ${num2} is: ${power}`);
-
 class Person{
     constructor(firstName, lastName, dob){
         this.firstName = firstName;
